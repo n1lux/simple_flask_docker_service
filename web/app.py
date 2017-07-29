1491dd7241ce37c0d1e8020f5a2dafad350c07c2
n1lux <n1lux.comp@gmail.com>
2017-07-29 11:02:15 -0300

    [*] Add ignore vim swap file, Create machine api resource

--- web/app.py
+++ web/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
+import platform
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!!!"
+    return "Machine Information api!!!"
+
+@app.route('/api/v0/machine')
+def machine_info():
+    p = platform.uname()
+    info = {'machine': p.machine,
+            'node': p.node,
+            'processor': p.processor,
+            'release': p.release,
+            'system': p.system,
+            'version': p.version
+            }
+
+    return jsonify(info)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
